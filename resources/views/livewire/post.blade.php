c82b4757efd94f43c6388332d681b3db9881342b
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-03-23 02:54:17 -0400

    Created page for single post

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -1,3 +1,14 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ $posts[0]->title }}
+    </h2>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+
+
+
+        </div>
+    </div>
 </div>
